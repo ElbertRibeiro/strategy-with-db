5e1e51d6427f05fa2779dc3953c97a99b4314c38
ElbertRibeiro <elbertprofissional@gmail.com>
2024-05-20 20:48:43 -0300

    feat: criando projeto

---
+++ src/main/java/elbertribeiro/com/strategy/model/Debito.java
@@ -0,0 +1,19 @@
+package elbertribeiro.com.strategy.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(schema = "EXEMPLO", name = "TAB_DEBITO")
+@Getter
+@Setter
+public class Debito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "DESCRICAO", columnDefinition = "VARCHAR")
+    private String descricao;
+}
