3dd686f2de029e97cf6cd58819d8741da4a8fe3c
ElbertRibeiro <elbertprofissional@gmail.com>
2024-05-21 07:52:18 -0300

    feat: criando projeto

--- src/main/java/elbertribeiro/com/service/DebitoCartaoCreditoService.java
+++ src/main/java/elbertribeiro/com/service/DebitoCartaoCreditoService.java
@@ -3,15 +3,18 @@ package elbertribeiro.com.service;
 import elbertribeiro.com.dto.DebitoRequest;
 import elbertribeiro.com.strategy.DebitoStrategy;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class DebitoCartaoCreditoService implements DebitoStrategy {
     private final DebitoService debitoService;
 
     @Override
     public void calculaJuros(DebitoRequest request) {
+        log.info("Calculando juros de debito de Cartao de Credito");
     }
 
     @Override
